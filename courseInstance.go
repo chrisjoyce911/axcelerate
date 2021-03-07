c6349042548313abba360f6b54e6ebc70e2ad484
Chris Joyce <chris@joyce.id.au>
2021-03-08 08:10:33 +1000

    Moved course-instances to own file

---
+++ courseInstance.go
@@ -0,0 +1,123 @@
+package axcelerate
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+// Instance of a course
+type Instance struct {
+	Cost                int64       `json:"COST"`
+	CustomfieldWeekends interface{} `json:"CUSTOMFIELD_WEEKENDS"`
+	Datedescriptor      string      `json:"DATEDESCRIPTOR"`
+	Enrolmentopen       bool        `json:"ENROLMENTOPEN"`
+	Finishdate          string      `json:"FINISHDATE"`
+	CoursesID           int64       `json:"ID"`
+	InstanceID          int64       `json:"INSTANCEID"`
+	Isactive            bool        `json:"ISACTIVE"`
+	Location            string      `json:"LOCATION"`
+	Maxparticipants     int64       `json:"MAXPARTICIPANTS"`
+	Minparticipants     int64       `json:"MINPARTICIPANTS"`
+	Name                string      `json:"NAME"`
+	Notices             interface{} `json:"NOTICES"`
+	Ownercontactid      int64       `json:"OWNERCONTACTID"`
+	Participants        int64       `json:"PARTICIPANTS"`
+	Participantvacancy  int64       `json:"PARTICIPANTVACANCY"`
+	Startdate           string      `json:"STARTDATE"`
+	Trainercontactid    int64       `json:"TRAINERCONTACTID"`
+	Virtualclassroomid  interface{} `json:"VIRTUALCLASSROOMID"`
+}
+
+// Attendeds for a Course Instance.
+type Attendeds struct {
+	Complexid  interface{} `json:"COMPLEXID"`
+	Date       string      `json:"DATE"`
+	Duration   interface{} `json:"DURATION"`
+	Enrollees  []Attended  `json:"ENROLLEES"`
+	Finishtime string      `json:"FINISHTIME"`
+	Instanceid int64       `json:"INSTANCEID"`
+	Starttime  string      `json:"STARTTIME"`
+	Type       string      `json:"TYPE"`
+}
+
+// Attended for a Course Instance.
+type Attended struct {
+	Attendanceid     interface{} `json:"ATTENDANCEID"`
+	Attendedduration interface{} `json:"ATTENDEDDURATION"`
+	Attendedflag     interface{} `json:"ATTENDEDFLAG"`
+	Comment          string      `json:"COMMENT"`
+	Completedflag    bool        `json:"COMPLETEDFLAG"`
+	Contactid        int64       `json:"CONTACTID"`
+	Enrolid          int64       `json:"ENROLID"`
+	Finishtime       string      `json:"FINISHTIME"`
+	Givenname        string      `json:"GIVENNAME"`
+	Starttime        string      `json:"STARTTIME"`
+	Surname          string      `json:"SURNAME"`
+}
+
+// GetCoursesInstances returns a list of instances
+func (c *CoursesService) GetCoursesInstances(coursesID int, active bool) ([]Instance, error) {
+	// lastUpdatedMin, lastUpdatedMax string
+	var instances []Instance
+
+	URL, error := url.Parse(c.client.baseURL.String())
+	URL.Path = "/api/course/instances"
+	if error != nil {
+		log.Fatal("An error occurs while handling url", error)
+	}
+	query := URL.Query()
+	query.Set("ID", fmt.Sprintf("%d", coursesID))
+	if active {
+		query.Set("current", "1")
+	} else {
+		query.Set("current", "0")
+	}
+
+	query.Set("type", "w")
+
+	URL.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", URL.String(), nil)
+	if err != nil {
+		return instances, err
+	}
+
+	resp, err := c.client.do(req, &instances)
+	if err != nil {
+		return instances, err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&instances)
+	return instances, err
+}
+
+// GetCoursesAttendeds for a Course Instance.
+func (c *CoursesService) GetCoursesAttendeds(instanceID int) ([]Attendeds, error) {
+	var attendeds []Attendeds
+
+	URL, error := url.Parse(c.client.baseURL.String())
+	URL.Path = "/api/course/instance/attendance"
+	if error != nil {
+		log.Fatal("An error occurs while handling url", error)
+	}
+	query := URL.Query()
+	query.Set("instanceID", fmt.Sprintf("%d", instanceID))
+
+	URL.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", URL.String(), nil)
+	if err != nil {
+		return attendeds, err
+	}
+
+	resp, err := c.client.do(req, &attendeds)
+	if err != nil {
+		return attendeds, err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&attendeds)
+	return attendeds, err
+}
