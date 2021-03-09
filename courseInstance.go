b4d311de22c16cfb2c0fbbf265657ff55033926e
Chris Joyce <chris@joyce.id.au>
2021-03-10 09:24:14 +1000

    Added GetCoursesInstanceDetail

--- courseInstance.go
+++ courseInstance.go
@@ -58,6 +58,39 @@ type Attended struct {
 	Surname          string      `json:"SURNAME"`
 }
 
+// InstanceDetail
+type InstanceDetail struct {
+	Complexdates        []interface{} `json:"COMPLEXDATES"`
+	Cost                int64         `json:"COST"`
+	CustomfieldWeekends interface{}   `json:"CUSTOMFIELD_WEEKENDS"`
+	Datedescriptor      string        `json:"DATEDESCRIPTOR"`
+	Enrolmentopen       bool          `json:"ENROLMENTOPEN"`
+	Finishdate          string        `json:"FINISHDATE"`
+	ID                  int64         `json:"ID"`
+	Instanceid          int64         `json:"INSTANCEID"`
+	Items               []interface{} `json:"ITEMS"`
+	Linkedclassid       int64         `json:"LINKEDCLASSID"`
+	Linkedelearning     []struct {
+		Code       string      `json:"CODE"`
+		Enddate    interface{} `json:"ENDDATE"`
+		Instanceid int64       `json:"INSTANCEID"`
+		Name       string      `json:"NAME"`
+		Startdate  interface{} `json:"STARTDATE"`
+	} `json:"LINKEDELEARNING"`
+	Location           string      `json:"LOCATION"`
+	Maxparticipants    int64       `json:"MAXPARTICIPANTS"`
+	Minparticipants    int64       `json:"MINPARTICIPANTS"`
+	Name               string      `json:"NAME"`
+	Notices            interface{} `json:"NOTICES"`
+	Ownercontactid     int64       `json:"OWNERCONTACTID"`
+	Participants       int64       `json:"PARTICIPANTS"`
+	Participantvacancy int64       `json:"PARTICIPANTVACANCY"`
+	Public             bool        `json:"PUBLIC"`
+	Startdate          string      `json:"STARTDATE"`
+	Trainercontactid   int64       `json:"TRAINERCONTACTID"`
+	Venuecontactid     int64       `json:"VENUECONTACTID"`
+}
+
 // GetCoursesInstances returns a list of instances
 func (c *CoursesService) GetCoursesInstances(coursesID int, active bool) ([]Instance, error) {
 	// lastUpdatedMin, lastUpdatedMax string
@@ -120,4 +153,35 @@ func (c *CoursesService) GetCoursesAttendeds(instanceID int) ([]Attendeds, error
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(&attendeds)
 	return attendeds, err
+}
+
+// GetCoursesInstanceDetail gets details of an Instance
+func (c *CoursesService) GetCoursesInstanceDetail(instanceID int, activityType string) (InstanceDetail, error) {
+	// lastUpdatedMin, lastUpdatedMax string
+	var details InstanceDetail
+
+	URL, error := url.Parse(c.client.baseURL.String())
+	URL.Path = "/api/course/instance/detail"
+	if error != nil {
+		log.Fatal("An error occurs while handling url", error)
+	}
+	query := URL.Query()
+	query.Set("instanceID", fmt.Sprintf("%d", instanceID))
+	query.Set("type", activityType)
+
+	URL.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", URL.String(), nil)
+	if err != nil {
+		return details, err
+	}
+
+	fmt.Println(req)
+	resp, err := c.client.do(req, &details)
+	if err != nil {
+		return details, err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&details)
+	return details, err
 }
