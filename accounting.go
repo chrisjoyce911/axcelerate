6009921fbe7ec49ec2ed3eb868907066136e5c70
Chris Joyce <chris@joyce.id.au>
2022-04-20 10:52:01 +1000

    Update Invoice and Payment

--- accounting.go
+++ accounting.go
@@ -12,19 +12,19 @@ type AccountingService struct {
 }
 
 type Payments struct {
-	Transactiondate       string `json:"TRANSACTIONDATE" time_format:"axc_date_hours"`
-	Transactionproviderid int    `json:"TRANSACTIONPROVIDERID"`
+	TransactionDate       string `json:"TRANSACTIONDATE" time_format:"axc_date_hours"`
+	TransactionProviderID int    `json:"TRANSACTIONPROVIDERID"`
 	GUID                  string `json:"GUID"`
-	Transactionprovider   string `json:"TRANSACTIONPROVIDER"`
+	TransactionProvider   string `json:"TRANSACTIONPROVIDER"`
 	FragmentAmount        int    `json:"FRAGMENT_AMOUNT"`
 }
 
 type Items struct {
-	Totaltax int `json:"TOTALTAX"`
+	TotalTax int `json:"TOTALTAX"`
 	Children struct {
 	} `json:"CHILDREN"`
 	Unitpricetax   int         `json:"UNITPRICETAX"`
-	DomainID      int         `json:"DOMAINID"`
+	DomainID       int         `json:"DOMAINID"`
 	HasChildren    int         `json:"HASCHILDREN"`
 	PartID         int         `json:"PARTID"`
 	Unitpricenett  int         `json:"UNITPRICENETT"`
@@ -44,58 +44,58 @@ type Items struct {
 
 // Invoice object with the full Invoice information
 type Invoice struct {
-	Invoicenr          string      `json:"INVOICENR"`
-	PriceGross         int         `json:"PRICEGROSS"`
-	Address2           interface{} `json:"ADDRESS2"`
-	OwnerContactID     int         `json:"OWNERCONTACTID"`
-	Organisation       interface{} `json:"ORGANISATION"`
-	ShipLastName       string      `json:"SHIPLASTNAME"`
-	Shiphousenr        string      `json:"SHIPHOUSENR"`
-	ShipOrgID          interface{} `json:"SHIPORGID"`
-	ShipPostCode       interface{} `json:"SHIPPOSTCODE"`
-	Comment            interface{} `json:"COMMENT"`
-	Phonenr            string      `json:"PHONENR"`
-	OrgID              interface{} `json:"ORGID"`
-	Payments           []Payments  `json:"PAYMENTS"`
-	Shipcountry        interface{} `json:"SHIPCOUNTRY"`
-	DueDate            string      `json:"DUEDATE" time_format:"axc_date"`
-	Shiporganisation   interface{} `json:"SHIPORGANISATION"`
-	Invoiceid          int         `json:"INVOICEID"`
-	Duedateoffset      int         `json:"DUEDATEOFFSET"`
-	Areitemslocked     bool        `json:"AREITEMSLOCKED"`
-	Lastname           string      `json:"LASTNAME"`
-	Street             string      `json:"STREET"`
-	Items              []Items     `json:"ITEMS"`
-	InvoiceDate        string      `json:"INVOICEDATE" time_format:"axc_date"`
-	Currency           string      `json:"CURRENCY"`
-	Shippricenett      int         `json:"SHIPPRICENETT"`
-	Shipstate          interface{} `json:"SHIPSTATE"`
-	Contactname        string      `json:"CONTACTNAME"`
-	Shipcountryiso3166 interface{} `json:"SHIPCOUNTRYISO3166"`
-	Shipstreet         string      `json:"SHIPSTREET"`
-	Housenr            string      `json:"HOUSENR"`
-	Isarchived         bool        `json:"ISARCHIVED"`
-	OrderDate          string      `json:"ORDERDATE" time_format:"axc_date"`
-	ContactID         int         `json:"CONTACTID"`
-	Shipcity           interface{} `json:"SHIPCITY"`
-	Shippricegross     int         `json:"SHIPPRICEGROSS"`
-	Countryiso3166     interface{} `json:"COUNTRYISO3166"`
-	Isinvoicenrlocked  bool        `json:"ISINVOICENRLOCKED"`
-	Shiptaxpercent     int         `json:"SHIPTAXPERCENT"`
-	Shippricetax       int         `json:"SHIPPRICETAX"`
-	State              interface{} `json:"STATE"`
-	Firstname          string      `json:"FIRSTNAME"`
-	Shipaddress2       interface{} `json:"SHIPADDRESS2"`
-	Shipfirstname      string      `json:"SHIPFIRSTNAME"`
-	City               interface{} `json:"CITY"`
-	Ordernr            interface{} `json:"ORDERNR"`
-	Email              string      `json:"EMAIL"`
-	Pricenett          int         `json:"PRICENETT"`
-	Country            interface{} `json:"COUNTRY"`
-	Invguid            string      `json:"INVGUID"`
-	Balance            int         `json:"BALANCE"`
-	Postcode           interface{} `json:"POSTCODE"`
-	IsPaid             bool        `json:"ISPAID"`
+	Invoicenr          string     `json:"INVOICENR"`
+	PriceGross         int        `json:"PRICEGROSS"`
+	Address2           string     `json:"ADDRESS2"`
+	OwnerContactID     int        `json:"OWNERCONTACTID"`
+	Organisation       string     `json:"ORGANISATION"`
+	ShipLastName       string     `json:"SHIPLASTNAME"`
+	Shiphousenr        string     `json:"SHIPHOUSENR"`
+	ShipOrgID          int        `json:"SHIPORGID"`
+	ShipPostCode       string     `json:"SHIPPOSTCODE"`
+	Comment            string     `json:"COMMENT"`
+	PhoneNumber        string     `json:"PHONENR"`
+	OrgID              int        `json:"ORGID"`
+	Payments           []Payments `json:"PAYMENTS"`
+	ShipCountry        string     `json:"SHIPCOUNTRY"`
+	DueDate            string     `json:"DUEDATE" time_format:"axc_date"`
+	ShipOrganisation   string     `json:"SHIPORGANISATION"`
+	InvoiceID          int        `json:"INVOICEID"`
+	Duedateoffset      int        `json:"DUEDATEOFFSET"`
+	Areitemslocked     bool       `json:"AREITEMSLOCKED"`
+	LastName           string     `json:"LASTNAME"`
+	Street             string     `json:"STREET"`
+	Items              []Items    `json:"ITEMS"`
+	InvoiceDate        string     `json:"INVOICEDATE" time_format:"axc_date"`
+	Currency           string     `json:"CURRENCY"`
+	Shippricenett      int        `json:"SHIPPRICENETT"`
+	ShipState          string     `json:"SHIPSTATE"`
+	Contactname        string     `json:"CONTACTNAME"`
+	ShipCountryISO3166 string     `json:"SHIPCOUNTRYISO3166"`
+	Shipstreet         string     `json:"SHIPSTREET"`
+	Housenr            string     `json:"HOUSENR"`
+	Isarchived         bool       `json:"ISARCHIVED"`
+	OrderDate          string     `json:"ORDERDATE" time_format:"axc_date"`
+	ContactID          int        `json:"CONTACTID"`
+	ShipCity           string     `json:"SHIPCITY"`
+	Shippricegross     int        `json:"SHIPPRICEGROSS"`
+	CountryISO3166     string     `json:"COUNTRYISO3166"`
+	Isinvoicenrlocked  bool       `json:"ISINVOICENRLOCKED"`
+	Shiptaxpercent     int        `json:"SHIPTAXPERCENT"`
+	Shippricetax       int        `json:"SHIPPRICETAX"`
+	State              string     `json:"STATE"`
+	FirstName          string     `json:"FIRSTNAME"`
+	Shipaddress2       string     `json:"SHIPADDRESS2"`
+	ShipFirstName      string     `json:"SHIPFIRSTNAME"`
+	City               string     `json:"CITY"`
+	OrderNumber        string     `json:"ORDERNR"`
+	Email              string     `json:"EMAIL"`
+	Pricenett          int        `json:"PRICENETT"`
+	Country            string     `json:"COUNTRY"`
+	Invguid            string     `json:"INVGUID"`
+	Balance            int        `json:"BALANCE"`
+	Postcode           string     `json:"POSTCODE"`
+	IsPaid             bool       `json:"ISPAID"`
 }
 
 // GetInvoice will get the details of an invoice / Update an invoice (NOTE: Currently you can only Lock Items and Finalise)
@@ -105,9 +105,9 @@ type Invoice struct {
 func (s *AccountingService) GetInvoice(invoiceID int) (Invoice, *Response, error) {
 	var obj Invoice
 
-	parms := map[string]string{"invoiceID": fmt.Sprintf("/%d",invoiceID), }
+	parms := map[string]string{"invoiceID": fmt.Sprintf("/%d", invoiceID)}
 
-	url :=fmt.Sprintf("/accounting/invoice/%d",invoiceID)
+	url := fmt.Sprintf("/accounting/invoice/%d", invoiceID)
 	log.Print(url)
 
 	resp, err := do(s.client, "GET", Params{parms: parms, u: url}, obj)
